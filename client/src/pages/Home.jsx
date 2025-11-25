adfcfba35a74ec4088b34371452c9143d8e6ad8f
KijinSeija265 <temp_denream@proton.me>
2025-11-25 15:15:10 +0800

    修改了主页面

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,11 @@
-import { Link } from 'react-router-dom';
+﻿import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import '../styles/Home.css';
 import UpdateAnnouncement from '../components/UpdateAnnouncement';
-import WelcomePopup from '../components/WelcomePopup';
 import announcements from '../data/announcements';
 
 const Home = () => {
   const [roomCount, setRoomCount] = useState(0);
-  const [showWelcomePopup, setShowWelcomePopup] = useState(false);
 
   useEffect(() => {
     const serverUrl = import.meta.env.VITE_SERVER_URL;
@@ -15,28 +13,18 @@ const Home = () => {
       .then(response => response.json())
       .then(data => setRoomCount(data.count))
       .catch(error => console.error('Error fetching room count:', error));
-    
-    // Show welcome popup when component mounts
-    setShowWelcomePopup(true);
   }, []);
-
-  const handleCloseWelcomePopup = () => {
-    setShowWelcomePopup(false);
-  };
 
   return (
     <div className="home-container">
-      {showWelcomePopup && (
-        <WelcomePopup onClose={handleCloseWelcomePopup} />
-      )}
-      
       <div className="game-modes">
         <Link to="/singleplayer" className="mode-button">
-          <h2>单人</h2>
+          <h2>单人修行</h2>
+          <small>与博丽神社的灵梦一起挑战幻想乡角色图鉴</small>
         </Link>
         <Link to="/multiplayer" className="mode-button">
-          <h2>多人</h2>
-          <small>当前房间数: {roomCount}</small>
+          <h2>联机竞速</h2>
+          <small>当前活跃结界：{roomCount}</small>
         </Link>
       </div>
       
@@ -49,20 +37,25 @@ const Home = () => {
       <div className="home-footer">
         <p>
           <a href="https://vertikarl.github.io/anime-character-guessr-english/"> ENGLISH ver. </a>
-          <br/>
-          一个猜动漫/游戏角色的网站，建议使用桌面端浏览器游玩。
-          <br/>
-          <a href="https://www.bilibili.com/video/BV14CVRzUELs">玩法简介视频</a>，灵感来源<a href="https://blast.tv/counter-strikle"> BLAST.tv </a>,
-          数据来源<a href="https://bgm.tv/"> Bangumi </a>。<br />
-          <a href="https://space.bilibili.com/87983557">@作者</a>："感谢 <a href="https://github.com/trim21">Bangumi 管理员</a> 的优化支持，
-          以及各位<a href="https://github.com/kennylimz/anime-character-guessr/graphs/contributors">网友</a>贡献的代码和数据。
-          感谢大家这段时间的热情和支持。"<br/>
-          想找朋友一起玩？QQ群：467740403<br/>
-          作者的新玩具：<a href="https://www.bilibili.com/video/BV1MstxzgEhg/">一个桌面挂件</a>
+          <br />
+          东方Project 角色猜谜特别版，推荐使用桌面端浏览器畅玩。
+          <br />
+          想快速了解玩法？可以观看
+          <a href="https://www.bilibili.com/video/BV14CVRzUELs"> 介绍视频 </a>，
+          灵感来源于 <a href="https://blast.tv/counter-strikle"> BLAST.tv </a> 的竞猜活动，
+          数据基于 <a href="https://bgm.tv/"> Bangumi </a> 与东方社区整合。
+          <br />
+          特别感谢 <a href="https://space.bilibili.com/87983557">@作者</a>、
+          <a href="https://github.com/trim21"> Bangumi 管理员</a>、
+          以及<a href="https://github.com/kennylimz/anime-character-guessr/graphs/contributors">所有贡献者</a>的协力。
+          <br />
+          想找同好一起结界挑战？QQ群：467740403
+          <br />
+          作者的新玩具：<a href="https://www.bilibili.com/video/BV1MstxzgEhg/"> 东方主题桌面挂件 </a>
         </p>
       </div>
     </div>
   );
 };
 
-export default Home; 
+export default Home;
